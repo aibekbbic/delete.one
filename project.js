7b90b5d44747ac0c027b6e8c7686850711864777
aibekbbic <129316740+aibekbbic@users.noreply.github.com>
2023-04-06 14:25:13 +0200

    Add files via upload

--- project.js
+++ project.js
@@ -1,11 +1,17 @@
-$(window).on("load", function () {
-  $("#header").vide("./video/cover", {
-    bgColor: "#908585",
-  });
+// header scroll
+let header = document.querySelector(".header");
+window.addEventListener("scroll", function () {
+  header.style.opacity = 1 - +window.pageYOffset / 700 + "";
 });
 
-$(document).ready(function () {
-  $("#checkbox").change(function () {
-    $("body").toggleClass("dark");
+// chevron down
+$(function () {
+  $("a[href*=#]").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      500,
+      "linear"
+    );
   });
 });
