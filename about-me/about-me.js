573bb1a1635059a8779f3e6753cd2fa4641ed8a8
aibekbbic <129316740+aibekbbic@users.noreply.github.com>
2023-05-15 21:28:10 +0200

    Delete about-me directory

--- about-me/about-me.js
+++
@@ -1,6 +0,0 @@
-let sidebarHidden = document.querySelector(".sidebar-hidden");
-
-function burgerMenu(icon) {
-  icon.classList.toggle("change");
-  sidebarHidden.classList.toggle("sidebar-hidden");
-}
